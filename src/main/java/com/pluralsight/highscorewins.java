2c7fbae325c15b027c87b6db604d69f9d2f87a93
Mari-art-rgb <meggisonariela@gmail.com>
2026-04-14 10:59:54 -0400

    Split the first part of the score entered by the user.

--- src/main/java/com/pluralsight/highscorewins.java
+++ src/main/java/com/pluralsight/highscorewins.java
@@ -1,4 +1,18 @@
 package com.pluralsight;
+import java.util.regex.Pattern;
+import java.util.Scanner;
 
 public class highscorewins {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter the score of Home and Visitor: ");
+        String score = scan.nextLine();
+
+        String[] splitScore = score.split(Pattern.quote("|"));
+        String firstTeam = splitScore[1];
+
+        System.out.println(firstTeam);
+
+    }
 }
