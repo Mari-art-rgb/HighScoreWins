727aa9b9365e5c63ef135e2a3ff7b4ee420a8daa
Mari-art-rgb <meggisonariela@gmail.com>
2026-04-14 11:18:38 -0400

    Split the first part of the score entered by the user. Print out Home & the last score but Visitor and second score is attached.

--- src/main/java/com/pluralsight/highscorewins.java
+++ src/main/java/com/pluralsight/highscorewins.java
@@ -9,10 +9,16 @@ public class highscorewins {
         System.out.println("Enter the score of Home and Visitor: ");
         String score = scan.nextLine();
 
-        String[] splitScore = score.split(Pattern.quote("|"));
-        String firstTeam = splitScore[1];
+        String[] splitScore = score.split(Pattern.quote(":"));
+
+        String firstTeam = splitScore[0];
+        String secondTeam = splitScore[1];
+        String numOne = splitScore[2];
+
 
         System.out.println(firstTeam);
+        System.out.println(secondTeam);
+        System.out.println(numOne);
 
     }
 }
